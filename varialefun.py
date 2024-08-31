76d0713eb7e1e7bc1b1d3cb8d70a0fa41d1bbd7f
Zubair Khan <zkhan15@mail.valenciacollege.edu>
2024-08-30 22:31:16 -0400

    Initial commit

---
+++ varialefun.py
@@ -0,0 +1,5 @@
+print("The answer to your query is",(5*6))
+print("The answer to your query is",(8/3))
+print("The answer to your query is",(8//3))
+print("The answer to your query is",(4200000+23090))
+print("The answer to your query is",(4200000-7633))
